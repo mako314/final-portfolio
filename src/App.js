11b8c4f41b99d7691afb2e5217e890a021d1ed4b
Macolister <bispo.mac@gmail.com>
2023-11-09 17:30:31 -0500

    project collection and cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import NavBar from './PortfolioComponents/NavBar';
 import AboutMe from './PortfolioComponents/AboutMe';
 import HomePageHero from './PortfolioComponents/HomePageHero';
+import ProjectCollection from './ProjectComponents/ProjectCollection';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <HomePageHero/>
+      <ProjectCollection/>
     </div>
   );
 }
---
+++ src/ProjectComponents/ProjectCollection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectCollection(){
+
+    return(
+        <div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+
+    <div class="mb-10 md:mb-16">
+      <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Selected</h2>
+
+      <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+    </div>
+
+
+    <div class="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
+      
+        <ProjectCard/>
+    </div>
+
+  </div>
+</div>
+    )
+}
+
+export default ProjectCollection
