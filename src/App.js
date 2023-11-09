51423514c482941dc7999b02e1161aa529ad63fa
Macolister <bispo.mac@gmail.com>
2023-11-09 18:17:17 -0500

    contact portion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './PortfolioComponents/NavBar';
 import AboutMe from './PortfolioComponents/AboutMe';
 import HomePageHero from './PortfolioComponents/HomePageHero';
 import ProjectCollection from './ProjectComponents/ProjectCollection';
+import Footer from './PortfolioComponents/Footer';
+import Contact from './PortfolioComponents/Contact';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar/>
       <HomePageHero/>
       <ProjectCollection/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
--- src/ProjectComponents/ProjectCard.js
+++ src/ProjectComponents/ProjectCard.js
@@ -6,10 +6,10 @@ function ProjectCard(){
         <a href="#" class="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
           <img src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700" loading="lazy" alt="Photo by Austin Wade" class="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
-          <div class="absolute left-0 bottom-2 flex gap-2">
+          {/* <div class="absolute left-0 bottom-2 flex gap-2">
             <span class="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">-50%</span>
             <span class="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">New</span>
-          </div>
+          </div> */}
         </a>
 
         <div class="flex items-start justify-between gap-2 px-2">
