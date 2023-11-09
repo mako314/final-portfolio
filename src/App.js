3b7ab848326235c56ec99d254c99b44ca695251c
Macolister <bispo.mac@gmail.com>
2023-11-09 17:23:34 -0500

    homepage hero in working on collections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './PortfolioComponents/NavBar';
 import AboutMe from './PortfolioComponents/AboutMe';
+import HomePageHero from './PortfolioComponents/HomePageHero';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <AboutMe/>
+      <HomePageHero/>
     </div>
   );
 }
