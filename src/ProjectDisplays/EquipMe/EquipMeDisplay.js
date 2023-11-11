8b0c8e5f0c49b2124e0003e68b3dcd2dd2e60934
Macolister <bispo.mac@gmail.com>
2023-11-11 15:14:54 -0500

    make image array functions to handle moving through it

--- src/ProjectDisplays/EquipMe/EquipMeDisplay.js
+++ src/ProjectDisplays/EquipMe/EquipMeDisplay.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import equipmeHome from '../../Assets/ProjectImages/EquipMe/equipmeHome.PNG'
 import EquipMeGallery from "./EquipMeGallery"
+
+const images = [
+    equipmeHome,
+    equipmeHome,
+    equipmeHome,
+    equipmeHome,
+  ]
 
 function EquipMeDisplay(){
 
     const [isModalOpen, setIsModalOpen] = useState(false)
     const [selectedImage, setSelectedImage] = useState(null)
-    
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
+    
     const openModal = (image) => {
         setSelectedImage(image)
     }
@@ -17,6 +25,32 @@ function EquipMeDisplay(){
         setSelectedImage(null)
     }
 
+    const goToPreviousImage = () => {
+        setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
+    }
+    
+      const goToNextImage = () => {
+        setSelectedIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }
+
+    useEffect(() => {
+        function handleKeyDown(event) {
+          if (!isModalOpen) return;
+          if (event.key === 'ArrowLeft') {
+            goToPreviousImage();
+          } else if (event.key === 'ArrowRight') {
+            goToNextImage();
+          }
+        }
+    
+        window.addEventListener('keydown', handleKeyDown);
+    
+        return () => {
+          window.removeEventListener('keydown', handleKeyDown);
+        };
+      }, [isModalOpen])
+
+
     return(
     <section>
         
@@ -25,31 +59,40 @@ function EquipMeDisplay(){
             <div className="grid grid-cols-1 items-center gap-8 sm:gap-20 lg:grid-cols-2">
 
             <div className="relative group cursor-pointer">
-            <img src={equipmeHome} alt="EquipMe home page" className="inline-block h-full w-full max-w-[640px] object-contain" />
+            <img src={equipmeHome} alt="EquipMe home page" className="rounded-xl inline-block h-full w-full max-w-[640px] object-contain" />
 
             {/* Overlay: hidden by default, shown on hover */}
-            <div onClick={openModal} className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity">
+            <div onClick={openModal} className="rounded-xl absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity">
             <p className="text-white text-xl border p-4 rounded-md">   View    </p>
             </div>
             </div>
-            
-            
 
             {selectedImage && (
             <div className="fixed top-0 left-0 h-full w-full flex items-center justify-center p-4 bg-black bg-opacity-50 z-50">
-            <div className="rounded-lg bg-white p-4 max-w-2xl max-h-full overflow-auto">
+            <div className=" relative rounded-lg bg-white p-4 max-w-2xl max-h-full overflow-auto">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>
+            
+            <button
+                onClick={closeModal}
+                className="absolute top-4 right-4mt-4 px-4 py-2 bg-red-500 text-white font-bold rounded hover:bg-red-700"
+                >
+                Close
+            </button>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
+
                 <img
                 src={equipmeHome} // The full-size image path
                 alt="EquipMe home page enlarged"
                 className="w-full h-auto"
                 />
-                <p> EquipMe Home Page</p>
-                <button
-                onClick={closeModal}
-                className="mt-4 px-4 py-2 bg-red-500 text-white font-bold rounded hover:bg-red-700"
-                >
-                Close
-                </button>
+                <p className="text-center"> EquipMe Home Page</p>
+                
             </div>
             </div>
             )}
--- src/ProjectDisplays/HobbyWarsDisplay.js
+++ src/ProjectDisplays/HobbyWarsDisplay.js
@@ -14,9 +14,10 @@ function HobbyWarsDisplay(){
             <div className="sm:max-w-sm md:max-w-md lg:max-w-lg">
                 <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9406b0e6cf62_placeholder-logo.svg" alt="" className="mb-4 inline-block" />
                 <p className="mb-6 max-w-md text-[#636262] md:mb-10 lg:mb-12"> Hobby Wars was founded on a powerful idea: to create a vibrant platform that connects enthusiasts from all walks of life to celebrate their passions through friendly competition and creative expression. Our inspiration for Hobby Wars stemmed from a shared love for hobbies and the realization that there's an extraordinary pool of talent waiting to be showcased.
-                <br><br></br></br>
+                <br></br><br></br>
+
                 Our mission is to make hobby participation more accessible and enjoyable for everyone. Whether you're an artist, chef, gardener, musician, or a hobbyist of any kind, Hobby Wars was designed with you in mind. We believe that by fostering a sense of community and camaraderie, we can inspire creativity, celebrate individual talents, and form lasting connections among our users.
-                <br><br></br></br>
+                <br></br><br></br>
                 Hobby Wars is more than just a platform; it's a vibrant community that empowers individuals to compete, connect, and shine. We are committed to simplifying the competition process, making it a fun and rewarding experience for everyone involved. Join us on this journey to unlock the full potential of your hobbies and to help others do the same.
                 </p>
                 <p className="font-bold">John Robert</p>
