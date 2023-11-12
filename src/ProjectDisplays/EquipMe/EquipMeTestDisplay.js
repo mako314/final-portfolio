e751c1733bb12972529ff7e0d0e141285ec1ed38
Macolister <bispo.mac@gmail.com>
2023-11-11 21:20:03 -0500

    button changes the picture

--- src/ProjectDisplays/EquipMe/EquipMeTestDisplay.js
+++ src/ProjectDisplays/EquipMe/EquipMeTestDisplay.js
@@ -56,10 +56,11 @@ function EquipMeTestDisplay({openModal, goToPreviousImage, goToNextImage, images
                 </p>
                 <p className="font-bold">John Robert</p>
                 <p className="text-sm text-[#636262]">Senior Webflow Developer</p>
+                <div className="flex justify-between items-center">
                   {/* Left arrow */}
                   <button
                   onClick={goToPreviousImage}
-                  className=" mt-4 left-4 top-1/2 transform -translate-y-1/2 text-white z-10"
+                  aria-label="Previous"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
@@ -69,12 +70,13 @@ function EquipMeTestDisplay({openModal, goToPreviousImage, goToNextImage, images
                 {/* Right arrow */}
                 <button
                   onClick={goToNextImage}
-                  className=" mt-4 ml-4 right-4 top-1/2 transform -translate-y-1/2 text-white z-10"
+                  aria-label="Next"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                   </svg>
                 </button>
+                </div>
             </div>
             </div>
   )
