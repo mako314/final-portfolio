cea6f9cc893458baae0997da3d89cf95f96e833b
Macolister <bispo.mac@gmail.com>
2024-04-29 21:00:05 -0400

    add adda to project cards

--- src/ProjectComponents/ProjectCard.js
+++ src/ProjectComponents/ProjectCard.js
@@ -6,6 +6,9 @@ import equipmeHome from '../Assets/ProjectImages/EquipMe/equipmeHome.PNG'
 //HobbyWar Photos
 import HobbyWarsHome from '../Assets/ProjectImages/HobbyWars/HobbyWarsHome.PNG'
 
+// Adda Photos
+import Adda from '../Assets/ProjectImages/Adda/Adda.PNG'
+
 function ProjectCard(){
     return(
       <>
@@ -46,6 +49,7 @@ function ProjectCard(){
           </div>
         </a>
 
+
         <div className="flex items-start justify-between gap-2 px-2">
           <div className="flex flex-col">
             <a href="https://github.com/mako314/HobbyWars" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">HobbyWars</a>
@@ -54,6 +58,34 @@ function ProjectCard(){
 
         </div>
       </div>
+
+
+
+      {/* PORTFOLIO SITE 3 */}
+
+      <div>
+        {/* https://youtu.be/ctFBd6pSzNA */}
+        {/* {Adda} */}
+
+        <a href="https://youtu.be/ctFBd6pSzNA" className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3">
+          <img src={Adda} loading="lazy" alt="Adda" className="h-full w-full object-contain object-center transition duration-200 group-hover:scale-110" />
+          <div className="absolute px-3 left-0 bottom-2 flex flex-wrap gap-2">
+            <span className="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">Next.js</span>
+            <span className="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">Python</span>
+            <span className="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">Flask</span>
+            <span className="rounded-lg bg-white px-1.5 py-0.5 text-sm font-bold uppercase tracking-wider text-gray-800">SQLAlchemy</span>
+            <span className="rounded-lg bg-white px-1.5 py-0.5 text-sm font-bold uppercase tracking-wider text-gray-800">OpenAI API</span>
+          </div>
+        </a>
+
+        <div className="flex items-start justify-between gap-2 px-2">
+          <div className="flex flex-col">
+            <a href="https://youtu.be/ctFBd6pSzNA" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">Adda</a>
+            <span className="text-gray-500">How my chatbot won "Most Innovative" in the hackathon. Adda is an educational chatbot who teaches students with disabilities, born out of a two-week hackathon. <br/> <br/> This link will take you to an external site (YouTube).</span>
+          </div>
+
+        </div>
+        </div>
 
       </>
     )
