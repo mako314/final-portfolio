faf64241455a7103febac1ffb24cd04dc821df2c
Macolister <bispo.mac@gmail.com>
2024-04-30 23:02:40 -0400

    nnetflix interactive hover

--- src/ProjectDisplays/NetflixClone/NetflixTemp.js
+++ src/ProjectDisplays/NetflixClone/NetflixTemp.js
@@ -2,6 +2,8 @@ import React from "react";
 import NotNetflixSeries from "../../Assets/ProjectImages/NetflixClone/NotNetflixTvSeries.PNG";
 import NotNetflixView from "../../Assets/ProjectImages/NetflixClone/NotNetflixTvView.PNG";
 import NotNetflixContWatching from "../../Assets/ProjectImages/NetflixClone/NotNetflxTvContinueWatchingArthur.PNG";
+import NotNetflixInteractiveHover from "../../Assets/ProjectImages/NetflixClone/NotNetflxTvMapped.PNG";
+
 
 
 
@@ -57,13 +59,14 @@ function NetflixTemp() {
         
         <div class="lg:w-1/3 sm:w-1/2 p-4">
           <div class="flex relative">
-            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://dummyimage.com/602x362"/>
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src={NotNetflixInteractiveHover}/>
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
-              <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Interactive Hover States</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">*Most "trailers" are House Season 1 trailers.</h1>
+              <p class="leading-relaxed"> Took advantage of Youtube embeds, mute enabled by default to save users experience!</p>
             </div>
           </div>
+          <p class="text-center mt-2 leading-relaxed">NotNetflix Interactive Hover</p>
         </div>
 
 
