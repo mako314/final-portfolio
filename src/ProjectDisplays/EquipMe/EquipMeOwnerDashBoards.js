89f48c67d45869ba9fdf93db725b4042f7f26cb7
Macolister <bispo.mac@gmail.com>
2024-04-30 23:45:03 -0400

    finish refactoring equipMe

--- src/ProjectDisplays/EquipMe/EquipMeOwnerDashBoards.js
+++ src/ProjectDisplays/EquipMe/EquipMeOwnerDashBoards.js
@@ -10,27 +10,28 @@ function EquipMeOwnerDash({openModal, goToPreviousPage, goToNextPage, images, se
         <div className="relative group cursor-pointer"> 
 
           <img src={EquipMeOwnerDashboardImage} alt="EquipMe Owner Dashboard" className="mx-auto h-auto max-w-full rounded-lg shadow-md" />
-          <div onClick={openModal} className="rounded-xl absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity"> 
+          <div onClick={() => openModal(EquipMeOwnerDashboardImage, "EquipMe Owner Dashboard", "EquipMe Owner Dashboard")} className="rounded-xl absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity"> 
             <p className="text-white text-xl border p-4 rounded-md">   View </p>
           </div>
 
 
-          {selectedImage && (
-        <div id="modal-overlay" className="fixed top-0 left-0 h-full w-full flex items-center justify-center p-4 bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
-          <div className="relative rounded-lg bg-white max-w-7xl max-h-full overflow-auto p-6" onClick={handleModalContentClick}>
-            <button onClick={closeModal} className="absolute top-0 right-0 mt-4 mr-4 px-2 py-2 bg-gray-300 rounded hover:bg-red-200">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-x-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-              </svg>
-            </button>
-            <img src={images[selectedIndex]} alt="EquipMe home page enlarged" className="w-full h-auto mx-auto" />
-            <div className="bg-gray-200 px-4 py-2 rounded-lg"> 
-              <p className="text-center"> EquipMe Onwer Dashboard </p>
-            </div> 
-          </div>
-        </div>
-      )}
+          {selectedImage?.src && (
+                <div id="modal-overlay" className="fixed top-0 left-0 h-full w-full flex items-center justify-center p-4 bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
+                <div className="relative rounded-lg bg-white max-w-7xl max-h-full overflow-auto p-6" onClick={handleModalContentClick}>
+                    <button onClick={closeModal} className="absolute top-0 right-0 mt-4 mr-4 px-2 py-2 bg-gray-300 rounded hover:bg-red-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-x-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                    </button>
+                    <img src={selectedImage.src} alt={selectedImage.altText} className="w-full h-auto mx-auto" />
+                    <div className="bg-gray-200 px-4 py-2 rounded-lg"> 
+                    <p className="text-center"> {selectedImage.infoText} </p>
+                    </div> 
+                </div>
+                </div>
+            )}
+
 
         </div>
           <h1 className="text-center text-3xl font-bold mt-4">EquipMe Owner Dashboard: Streamline Your Equipment Rental Business</h1>
