9af2a42ee1715e89718a3aab122d155a11c0194d
Macolister <bispo.mac@gmail.com>
2024-04-30 22:22:22 -0400

    nextflix clone movie collection page

--- src/ProjectDisplays/NetflixClone/NetflixCloneTemp.js
+++ src/ProjectDisplays/NetflixClone/NetflixCloneTemp.js
@@ -1,17 +1,17 @@
 import React from "react";
-import NetflixMovies from "../../Assets/ProjectImages/NetflixClone/NotNetflixMovies.PNG";
+import NotNetflixMovies from "../../Assets/ProjectImages/NetflixClone/NotNetflixMovies.PNG";
 
 function NetflixCloneTemp() {
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container">
         <div className="lg:w-4/5 mx-auto flex flex-wrap items-center justify-center">
-          <div className="lg:w-1/2 w-full lg:pr-10 mb-2 lg:mb-0">
-            <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-800 mb-4">
-              Welcome to Your Netflix Clone
+        <div className="lg:w-full w-full lg:pr-10 mb-2 lg:mb-0">
+            <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-800 mb-4 text-center">
+              Definitely Not Netflix
             </h2>
-            <p className="text-lg leading-relaxed text-gray-700">
-              Here you can explore a collection of movies just like on Netflix.
+            <p className="text-lg lg:text-xl leading-relaxed text-gray-700 text-center">
+              Here we offer a collection of movies, something never done before. A whole collection of films? We are also considering branching into "DNN Originals"
               Enjoy your favorite films and shows!
             </p>
           </div>
@@ -19,7 +19,7 @@ function NetflixCloneTemp() {
               <img
                 alt="Netflix movies"
                 className="w-full h-full object-cover object-center"
-                src={NetflixMovies}
+                src={NotNetflixMovies}
               />
           </div>
         </div>
