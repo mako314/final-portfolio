b43ba53f49222c4269dd7e0d21b34c71b32f6cc0
Macolister <bispo.mac@gmail.com>
2024-05-04 03:47:59 -0400

    dark mode theme addition

---
+++ src/PortfolioComponents/DarkModeContext.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+// Create context object
+export const ThemeContext = React.createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
