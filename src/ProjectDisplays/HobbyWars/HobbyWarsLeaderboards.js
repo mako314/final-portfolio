416434afb87eb84583b6737c889cbf4f23d2e4c3
macolisterbispo <bispo.mac@gmail.com>
2024-05-05 14:20:38 -0400

    slide 2 hobby wars dark mode

--- src/ProjectDisplays/HobbyWars/HobbyWarsLeaderboards.js
+++ src/ProjectDisplays/HobbyWars/HobbyWarsLeaderboards.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, {useContext} from "react"
 import HobbyWarsLeaderBoardsImage from '../../Assets/ProjectImages/HobbyWars/HobbyWarsLeaderBoards.PNG';
+import { ThemeContext } from '../../PortfolioComponents/ThemeContext';
 
 function HobbyWarsLeaderBoards({
   openModal, goToPreviousPage, goToNextPage, images, selectedIndex, selectedImage, closeModal, handleModalContentClick, handleOverlayClick
 }) {
-  console.log(goToNextPage);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
+    <section className="body-font overflow-hidden">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-center">
-          <h2 className="title-font sm:text-3xl text-2xl font-medium text-black mb-4 text-center">
+          <h2 className={`title-font sm:text-3xl text-2xl font-medium text-${theme === 'dark' ? 'white' : 'black'} mb-4 text-center`}>
             Real Time Leaderboards
           </h2>
           <div className="w-full group cursor-pointer"> {/* Parent container for image and overlay */}
             <div className="relative shadow-lg"> {/* This div becomes the relative container */}
               <img alt="Leaderboard display" className="object-cover object-center w-full h-auto" src={HobbyWarsLeaderBoardsImage} />
-              <div onClick={() => openModal(HobbyWarsLeaderBoardsImage, "Hobby Wars Leaderboards Page", "Hobby Wars Leader Boards")} className="rounded-xl absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity">
+              <div onClick={() => openModal(HobbyWarsLeaderBoardsImage, "Hobby Wars Leaderboards Page", "Hobby Wars Leader Boards")} className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center group-hover:opacity-100 opacity-0 transition-opacity">
                 <p className="text-white text-xl border p-4 rounded-md">View</p>
               </div>
             </div>
           </div>
           {selectedImage?.src && (
                 <div id="modal-overlay" className="fixed top-0 left-0 h-full w-full flex items-center justify-center p-4 bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
-                <div className="relative rounded-lg bg-white max-w-7xl max-h-full overflow-auto p-6" onClick={handleModalContentClick}>
+                <div className={`relative rounded-lg bg-${theme === 'dark' ? 'gray-800' : 'white'} max-w-7xl max-h-full overflow-auto p-6`} onClick={handleModalContentClick}>
                     <button onClick={closeModal} className="absolute top-0 right-0 mt-4 mr-4 px-2 py-2 bg-gray-300 rounded hover:bg-red-200">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-x-circle" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -30,7 +31,7 @@ function HobbyWarsLeaderBoards({
                     </svg>
                     </button>
                     <img src={selectedImage.src} alt={selectedImage.altText} className="w-full h-auto mx-auto" />
-                    <div className="bg-gray-200 px-4 py-2 rounded-lg"> 
+                    <div className={`bg-${theme === 'dark' ? 'gray-300' : 'gray-200'} mt-2 px-4 py-2 rounded-lg`}> 
                     <p className="text-center"> {selectedImage.infoText} </p>
                     </div> 
                 </div>
@@ -45,7 +46,7 @@ function HobbyWarsLeaderBoards({
                   aria-label="Previous"
                   className='mr-5'
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="black" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : 'black'}`} className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                   </svg>
                 </button>
@@ -55,13 +56,13 @@ function HobbyWarsLeaderBoards({
                   onClick={goToNextPage}
                   aria-label="Next"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="black" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : 'black'}`} className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                   </svg>
                 </button>
       </div>
       <a href="/projects" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
-                        <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Back to Projects</button>
+                        <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-[#88BDBC] rounded-lg lg:w-auto hover:bg-[#76a9a3] focus:outline-none ">Back to Projects</button>
                         
     </a>
     </section>
