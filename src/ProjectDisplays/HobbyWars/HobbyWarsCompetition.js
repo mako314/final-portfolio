b6cbc9cff7c2c67a9a018bf830d7fe56419e2286
macolisterbispo <bispo.mac@gmail.com>
2024-05-05 21:57:29 -0400

    hobby wars button link not a element

--- src/ProjectDisplays/HobbyWars/HobbyWarsCompetition.js
+++ src/ProjectDisplays/HobbyWars/HobbyWarsCompetition.js
@@ -1,4 +1,5 @@
 import React, {useContext} from "react"
+import {Link} from 'react-router-dom';
 import HobbyWarsChallenge from '../../Assets/ProjectImages/HobbyWars/HobbyWarsChallenge.PNG'
 import { ReactComponent as HobbyWarLogo } from '../../Assets/ProjectImages/HobbyWars/Logo/HobbyWarLogo.svg'
 import { ThemeContext } from '../../PortfolioComponents/ThemeContext';
@@ -75,10 +76,10 @@ function HobbyWarsCompetition({openModal, goToPreviousPage, goToNextPage, images
                 </button>
           </div>
         </div>
-        <a href="/projects" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
+        <Link to="/projects" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
                         <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-[#88BDBC] rounded-lg lg:w-auto hover:bg-[#76a9a3] focus:outline-none ">Back to Projects</button>
                         
-    </a>
+    </Link>
 
     </section>
     )
--- src/ProjectDisplays/HobbyWars/HobbyWarsUserProfiles.js
+++ src/ProjectDisplays/HobbyWars/HobbyWarsUserProfiles.js
@@ -1,4 +1,5 @@
 import React, {useContext} from "react"
+import {Link} from 'react-router-dom';
 import HobbyWarsUserProfileImage from '../../Assets/ProjectImages/HobbyWars/HobbyWarsUserProfile.PNG';
 import UserProfileChallenges from '../../Assets/ProjectImages/HobbyWars/UserProfileChallenges.PNG';
 import UserProfileHobbies from '../../Assets/ProjectImages/HobbyWars/UserProfileHobbies.PNG';
@@ -126,10 +127,10 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, image
 </div>
 
 
-<a href="/projects" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
+<Link to="/projects" className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
                         <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-[#88BDBC] rounded-lg lg:w-auto hover:bg-[#76a9a3] focus:outline-none ">Back to Projects</button>
                         
-    </a>
+    </Link>
 
 
 
