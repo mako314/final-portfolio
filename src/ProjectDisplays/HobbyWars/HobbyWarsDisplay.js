6573750da8d609c26eb18fa34893b9c8afa6d558
Macolister <bispo.mac@gmail.com>
2024-05-14 16:10:55 -0400

    style(HobbyWars): remove all dark: change btn clrs

--- src/ProjectDisplays/HobbyWars/HobbyWarsDisplay.js
+++ src/ProjectDisplays/HobbyWars/HobbyWarsDisplay.js
@@ -91,7 +91,7 @@ function HobbyWarsDisplay(){
     const SelectedComponent = projectDisplayComponents[selectedIndex]
     
     return( 
-        <div className={`bg-${theme === 'dark' ? 'gray-800' : 'white'} dark:bg-gray-800 mx-auto max-w-full px-5 py-16 md:px-10`}>
+        <div  className={`flex flex-row justify-center items-center bg-${theme === 'dark' ? 'gray-800' : 'gray-100'} min-w-full px-12 py-24 md:px-10`}>
           <SelectedComponent  
           openModal={openModal} 
           goToPreviousPage={goToPreviousPage} 
--- src/ProjectDisplays/HobbyWars/HobbyWarsUserProfiles.js
+++ src/ProjectDisplays/HobbyWars/HobbyWarsUserProfiles.js
@@ -25,7 +25,7 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, selec
                     </div>
                     {selectedImage?.src && (
                 <div id="modal-overlay" className="fixed top-0 left-0 h-full w-full flex items-center justify-center p-4 bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
-                <div className={`relative rounded-lg bg-${theme === 'dark' ? 'gray-800' : 'white'} dark:bg-gray-800 max-w-7xl max-h-full p-6`} onClick={handleModalContentClick}>
+                <div className={`relative rounded-lg bg-${theme === 'dark' ? 'gray-800' : 'gray-100'} max-w-7xl max-h-full p-6`} onClick={handleModalContentClick}>
                     <button onClick={closeModal} className="absolute top-0 right-0 mt-4 mr-4 px-2 py-2 bg-gray-300 rounded hover:bg-red-200">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-x-circle" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -33,7 +33,7 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, selec
                     </svg>
                     </button>
                     <img src={selectedImage.src} alt={selectedImage.altText} className="w-full h-auto mx-auto" />
-                    <div className={`bg-${theme === 'dark' ? 'gray-300' : 'gray-200'} dark:bg-gray-300 mt-2 px-4 py-2 rounded-lg`}> 
+                    <div className={`bg-${theme === 'dark' ? 'gray-300' : 'gray-200'} mt-2 px-4 py-2 rounded-lg`}> 
                     <p className="text-center"> {selectedImage.infoText} </p>
                     </div> 
                 </div>
@@ -48,14 +48,14 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, selec
     <div className="flex"> {/* Flex container */}
     {/* Left Column for Profile Name and Role */}
     <div className="flex-initial" style={{ flexBasis: '15%' }}> {/* Takes up 60% width */}
-    <p className={`text-${theme === 'dark' ? 'white' : 'black'} dark:text-white font-bold`}><HobbyWarLogo/> Max Adler - </p>
-    <p className={`text-sm text-${theme === 'dark' ? 'white' : '[#636262]'} dark:text-white font-bold`}>TOTALLY Real Human</p>
+    <p className={`text-${theme === 'dark' ? 'white' : 'gray-800'}  font-bold`}><HobbyWarLogo/> Max Adler - </p>
+    <p className={`text-sm text-${theme === 'dark' ? 'white' : 'gray-700'}  font-bold`}>TOTALLY Real Human</p>
     </div>
 
         {/* Right Column for Character Description */}
         <div className="flex-auto" style={{ flexBasis: '85%' }}> {/* Takes up remaining 40% width */}
-            <div className="p-4 bg-gray-100 rounded-md shadow">
-                <p className="text-center text-[#636262]">
+            <div className={`p-4 bg-${theme === 'dark' ? 'gray-100':'gray-200'} rounded-md shadow`}>
+                <p className={`text-center ${theme === 'dark' ? 'text-gray-700':'text-gray-600'}  `}>
                     Max is really happy that this platform has given him a place to host his challenge and find a community of fellow star gazers. I most definitely did not make them up, nor sponsor them for this post.
                 </p>
             </div>
@@ -110,7 +110,7 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, selec
                   aria-label="Previous"
                   className='mr-5'
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : 'black'}`} className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : '#34568B'}`} className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                   </svg>
                 </button>
@@ -120,7 +120,7 @@ function HobbyWarsUserProfile({ openModal, goToPreviousPage, goToNextPage, selec
                   onClick={goToNextPage}
                   aria-label="Next"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : 'black'}`} className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill={`${theme === 'dark' ? 'white' : '#34568B'}`} className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                   </svg>
                 </button>
